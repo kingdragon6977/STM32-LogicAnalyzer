d9f4a565cf192d21710d9009cc63d12d9031283f
kingdragon6977 <53393991+kingdragon6977@users.noreply.github.com>
2026-08-04 04:23:21 -0400

    Expose triggered sampler API

--- inc/sampler.h
+++ inc/sampler.h
@@ -2,7 +2,9 @@
 #define SAMPLER_H
 
 #include <stdint.h>
+
 #define CAPTURE_SAMPLES 16384
+
 void sampler_init(void);
 
 void sampler_start(
@@ -10,7 +12,15 @@ void sampler_start(
     uint32_t count
 );
 
+void sampler_start_triggered(
+    uint8_t *buf,
+    uint32_t count,
+    uint8_t channel,
+    uint8_t rising
+);
+
 uint8_t sampler_done(void);
+
 void sampler_set_rate(uint32_t hz);
 
 uint8_t *sampler_get_buffer(void);

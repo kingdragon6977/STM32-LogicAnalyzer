995593c9dd8a92f7b8cedaf67af844340534c78d
kingdragon6977 <53393991+kingdragon6977@users.noreply.github.com>
2026-08-17 02:45:56 -0400

    I2C snoop: add 4 MHz capture rate

--- inc/capture.h
+++ inc/capture.h
@@ -3,15 +3,11 @@
 
 #include <stdint.h>
 
-
 typedef enum
 {
     MODE_EDGE,
     MODE_I2C
-
 } analyzer_mode_t;
-
-
 
 typedef enum
 {
@@ -19,54 +15,20 @@ typedef enum
     RATE_100K,
     RATE_500K,
     RATE_1M,
-    RATE_2M
+    RATE_2M,
+    RATE_4M
 } capture_rate_t;
 
-
-
 void capture_init(void);
-
-
 void capture_run(void);
-
-
 void capture_raw(void);
-
-
-
-void capture_set_mode(
-    analyzer_mode_t mode
-);
-
-
+void capture_set_mode(analyzer_mode_t mode);
 analyzer_mode_t capture_get_mode(void);
-
-
-
-void capture_set_rate(
-    uint32_t hz
-);
-
-
+void capture_set_rate(uint32_t hz);
 uint32_t capture_get_rate(void);
-
-
-
-void capture_set_rate_enum(
-    capture_rate_t rate
-);
-
-
-
-void capture_set_trigger(
-    uint8_t channel,
-    uint8_t rising
-);
-
-
-/* select capture backend: 1 = DMA, 0 = IRQ sampler */
+void capture_set_rate_enum(capture_rate_t rate);
+void capture_set_trigger(uint8_t channel, uint8_t rising);
 void capture_set_backend_dma(uint8_t enable);
 uint8_t capture_get_backend_dma(void);
-
 
 #endif
